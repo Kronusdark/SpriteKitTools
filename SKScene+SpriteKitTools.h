b48d8048d07d3956122ba39139572056e19ee594
Weston Hanners <weston@hanners.us>
2014-03-04 13:57:07 -0500

    Added a category to setup default frame for an SKScene

---
+++ SKScene+SpriteKitTools.h
@@ -0,0 +1,15 @@
+//
+//  SKScene+SpriteKitTools.h
+//  Sprite Kit Test
+//
+//  Created by Weston Hanners on 3/4/14.
+//  Copyright (c) 2014 Weston Hanners. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SKScene (SpriteKitTools)
+
+- (void)initDefaultFramePhysics;
+
+@end
---
+++ SKScene+SpriteKitTools.m
@@ -0,0 +1,21 @@
+//
+//  SKScene+SpriteKitTools.m
+//  Sprite Kit Test
+//
+//  Created by Weston Hanners on 3/4/14.
+//  Copyright (c) 2014 Weston Hanners. All rights reserved.
+//
+
+#import "SKScene+SpriteKitTools.h"
+
+@implementation SKScene (SpriteKitTools)
+
+- (void)initDefaultFramePhysics {
+    
+    SKPhysicsBody *framePhysics = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    
+    [self setPhysicsBody:framePhysics];
+    
+}
+
+@end
