77ac9c2c2d91164bf7dd2534e83c8fda44acffcc
Weston Hanners <weston@hanners.us>
2014-03-04 14:13:10 -0500

    Moved Categories and Added a master header.

---
+++ SpriteKitTools.h
@@ -0,0 +1,12 @@
+//
+//  SpriteKitTools.h
+//
+//  Created by Weston Hanners on 3/4/14.
+//  Copyright (c) 2014 Weston Hanners. All rights reserved.
+//
+
+    // Master Import Header
+
+#import "SKSpriteNode+SpriteKitTools.h"
+#import "SKEmitterNode+SpriteKitTools.h"
+#import "SKScene+SpriteKitTools.h"
