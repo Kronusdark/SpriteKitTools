647ffcbbd9bdbfa2cffc8bab61c6d110ebea8f10
Weston Hanners <weston@hanners.us>
2014-03-04 14:14:44 -0500

    Removed Project Name from file header.

--- Categories/SKEmitterNode+SpriteKitTools.m
+++ Categories/SKEmitterNode+SpriteKitTools.m
@@ -1,6 +1,5 @@
 //
 //  SKEmitterNode+SpriteKitTools.m
-//  Sprite Kit Test
 //
 //  Created by Weston Hanners on 3/4/14.
 //  Copyright (c) 2014 Weston Hanners. All rights reserved.
--- Categories/SKScene+SpriteKitTools.h
+++ Categories/SKScene+SpriteKitTools.h
@@ -1,6 +1,5 @@
 //
 //  SKScene+SpriteKitTools.h
-//  Sprite Kit Test
 //
 //  Created by Weston Hanners on 3/4/14.
 //  Copyright (c) 2014 Weston Hanners. All rights reserved.
