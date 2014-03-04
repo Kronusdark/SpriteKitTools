15b9954b9296df554c6e6f7e1b962bfc7e3900ec
Weston Hanners <weston@hanners.us>
2014-03-04 14:23:40 -0500

    Added Constants
    Added SpriteKitToolsConstants.h to keep track of keyboard scan codes. I
    am sure I will think of something else to keep in here at some point in
    the future.

---
+++ SpriteKitToolsConstants.h
@@ -0,0 +1,15 @@
+//
+//  SpriteKitToolsConstants.h
+//
+//  Created by Weston Hanners on 3/4/14.
+//  Copyright (c) 2014 Weston Hanners. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+unsigned short const skt_keyDown = 125;
+unsigned short const skt_keyLeft = 123;
+unsigned short const skt_keyRight = 124;
+unsigned short const skt_keyUp = 126;
+unsigned short const skt_keySpace = 49;
+
