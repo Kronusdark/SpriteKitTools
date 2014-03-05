b7067a388876343974a812548287db8e9052594a
Weston Hanners <weston@hanners.us>
2014-03-04 21:19:12 -0500

    Added methods for subtraction and division of CGPoints

--- SpriteKitToolsFunctions.h
+++ SpriteKitToolsFunctions.h
@@ -20,4 +20,22 @@ CGPoint skt_CGPointMultiply(CGPoint pointA, CGPoint pointB) {
                                    pointA.y * pointB.y);
     return newPoint;
 }
+
+    /// Subtracts pointA from pointB
+CGPoint skt_CGPointSubtract(CGPoint pointA, CGPoint pointB) {
+    CGPoint newPoint = CGPointMake(pointA.x - pointB.x,
+                                   pointA.y - pointB.y);
+    return newPoint;
+}
+
+    /// Divides pointA by pointB
+CGPoint skt_CGPointDivide(CGPoint pointA, CGPoint pointB) {
+    
+    if (pointB.x == 0 || pointB.y == 0)
+        return CGPointZero;
+    
+    CGPoint newPoint = CGPointMake(pointA.x / pointB.x,
+                                   pointA.y / pointB.y);
+    return newPoint;
+}
 }
